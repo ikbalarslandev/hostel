64cca443a4ff15c2b188271dfbe074b6f3eaff30
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-02-22 15:36:26 +0300

    Nav menu mobile setup

--- components/nav.tsx
+++ components/nav.tsx
@@ -12,7 +12,7 @@ const Nav = () => {
     setMenuOpen(!isMenuOpen);
   };
   return (
-    <nav className="bg-black py-2 flex items-center relative justify-between pl-1 h-12 m-0">
+    <nav className="bg-gray-900 py-2 flex items-center  relative justify-between pl-1 h-12 m-0">
       {/* hamburger icon */}
       <div className="md:hidden ml-1 mt-2">
         <button
@@ -33,12 +33,17 @@ const Nav = () => {
         <Image
           src="/logo.png"
           alt="logo"
-          width={150}
+          width={210}
           height={800}
-          className="ml-5"
+          className="mb-1"
         />
       </Link>
-      <Button className="bg-pink rounded-none h-12 p-1 ">Book Your Stay</Button>
+      <Button className="bg-pink rounded-none h-12 p-1 text-xs ">
+        Book Your Stay
+      </Button>
+      {isMenuOpen && (
+        <div className="bg-pink absolute top-12 left-0 h-screen min-w-[290px] animate-fade-right z-10 "></div>
+      )}
     </nav>
   );
 };
