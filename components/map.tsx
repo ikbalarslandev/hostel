5b5a04d8f4bdfddad4d6c812c14575051ac0ad13
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-02-27 20:13:29 +0300

    Bug fix map

--- components/map.tsx
+++ components/map.tsx
@@ -9,14 +9,14 @@ import {
   InfoWindow,
 } from "@vis.gl/react-google-maps";
 
-export default function MapPage({ className }: { className?: string }) {
+export default function MapPage({ className = "" }: { className?: string }) {
   const position = { lat: 41.0367, lng: 28.985 };
   const [open, setOpen] = useState(false);
 
   return (
     process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY && (
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-        <section className={`${className}`}>
+        <section className={`${className} `}>
           <Map
             zoom={12}
             center={position}
