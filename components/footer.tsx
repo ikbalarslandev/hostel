9785fd5cc0718ffab327c6632e660a8d2bc91250
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-02-28 12:42:30 +0300

    Responsive footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,21 +4,35 @@ import { FaInstagram } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
-  const iconClass = "w-10 h-10 bg-yellow-400 m-1 p-2 rounded-full";
+  const iconClass =
+    "w-10 md:w-9 md:h-9 h-10 bg-yellow-400 m-1 p-2 rounded-full";
   return (
-    <footer className="h-20 bg-black flex  ">
-      <div className=" flex-1 flex flex-col items-start ml-2 pt-1 gap-1 justify-center">
+    <footer className="h-20 md:h-12 bg-black flex items-center justify-between px-1">
+      {/* logo */}
+      <div className=" flex items-center flex-col md:flex-row md:gap-8 ">
         <p className="font-bold italic text-4xl text-pink">FAQ</p>
+        <p className="text-yellow-500 font-bold hidden md:block">
+          Privacy Policy
+        </p>
         <p className="text-yellow-500 font-bold">Contact us</p>
       </div>
-      <div className=" flex flex-col items-top  justify-center mx-2 mt-1   ">
-        <div className="flex mr-auto">
+
+      {/* icons */}
+      <div className=" flex flex-col items-end  justify-center  mt-1   ">
+        <div className="flex mr-auto md:mr-0">
           <FaTripadvisor className={iconClass} />
           <FaTwitter className={iconClass} />
           <FaInstagram className={iconClass} />
           <FaFacebookF className={iconClass} />
         </div>
-        <p className=" text-end font-bold text-yellow-400">Privacy Policy</p>
+        <p className=" text-end font-bold text-yellow-400 md:hidden  ">
+          Privacy Policy
+        </p>
+      </div>
+
+      <div className="gap-3  hidden md:flex mr-2  ">
+        <p className="text-yellow-400 font-bold">Şehit Muhtar str.</p>
+        <p className="text-yellow-400 ">Istanbul</p>
       </div>
     </footer>
   );
